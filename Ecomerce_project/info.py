121b29f39e0042f36827dcb7cc252329bc7f2dee
Ecommerce695 <110809060+Ecommerce695@users.noreply.github.com>
2022-09-22 10:09:09 +0530

    Update info.py
    Required Fields Email and Password

--- Ecomerce_project/info.py
+++ Ecomerce_project/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'pakshay@stratapps.com'  # Pass Sender Email Address
-EMAIL_HOST_PASSWORD = 'AKShay@stratapps'   # Pass Sender Email Password
+EMAIL_HOST_USER = 'example@gmail.com'  # Pass Sender Email Address
+EMAIL_HOST_PASSWORD = 'Enter Password'   # Pass Sender Email Password
 EMAIL_PORT = 587
