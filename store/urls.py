fd32495e440523667fe4ddbc443d8d747c1bd31c
Ecommerce695 <110809060+Ecommerce695@users.noreply.github.com>
2022-09-27 11:55:25 +0530

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -6,16 +6,8 @@ urlpatterns = [
     path('signup/', views.register_api, name='signup'),
     path('login/', views.login_api, name='login'),
     path('logout/', views.signoff, name='logout'),
-#     # to Fetch All Records from the DB
-#     path('details/', views.details, name='details'),
-#     # This Url is used to Update the User Details
     path('user_detail/<uuid:pk>',views.user_detail, name= 'user_detail'),
     path('address/<uuid:id>/',views.address_api, name='address'),
     path('wish_list/<uuid:id>/<int:pid>/',views.wish_list,name='wish_list'),
-#     path('product/',views.product_view, name='product'),
-#     path('orders/<int:id>/<int:pid>/',views.my_order,name='orders'),
-#     path('myorders/<int:id>/',views.purchase),
-#     # re_path(r'search/(?P<cat>\D+)',views.search_bar,name='search'),
-#     # path('fetch/<int:id>', views.fetch)
 
 ]
