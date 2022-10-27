3a70a4682937cbd0ccb6e135355f215eacf11825
Ecommerce695 <110809060+Ecommerce695@users.noreply.github.com>
2022-10-27 16:20:22 +0530

    Update views.py

--- store/views.py
+++ store/views.py
@@ -126,35 +126,7 @@ def reset_pwd_api(request,token):
         data = list(cust.values('first_name', 'last_name','username'))
         return JsonResponse(data, safe=False)
         
-# # Modifing First Name 
-# @transaction.atomic
-# @api_view(['PUT'])
-# def fname_update_api(request, id):
-#     if request.method =='PUT':
-#         first_name = request.POST['first_name']
-#         CustomerProfile.objects.filter(id=id).update(first_name = first_name)
-#         return HttpResponse("Modified First Name")
-
-# #Modifing Last Name 
-# @transaction.atomic
-# @api_view(['PUT'])
-# def lname_update_api(request, id):
-#     if request.method =='PUT':
-#         last_name = request.POST['last_name']
-#         CustomerProfile.objects.filter(id=id).update(last_name = last_name)
-#         return HttpResponse("Modified Last Name")
-
-# #Modifing Email
-# @transaction.atomic
-# @api_view(['PUT'])
-# def email_update_api(request,id):
-#     if request.method == 'PUT':
-#         update_email = request.POST['email']
-    
-#         CustomerProfile.objects.filter(id=id).update(email = update_email)
-#         return HttpResponse("Modified Email")
-
-
+        
 # Moving Fav Product to WishList
 @transaction.atomic
 @api_view(['POST'])
